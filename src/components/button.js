4eb652fd82fa1c51e56c532d99fa9dfbd8d18e42
Elite-sxf <1399826164@qq.com>
2022-10-09 18:19:18 +0800

    first commit

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import App from "../App";
+
+export default class Abutton extends React.Component {
+    render() {
+        return (
+            <button>这是一个按钮</button>
+        )
+    }
+}
